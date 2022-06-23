1b7da651543754d13cb99ae6f24dce483afce7a7
mangalan <57430796+mangalan516@users.noreply.github.com>
2022-06-23 14:01:35 +0530

    Changed some codes
    Updated code with some features

--- IpScanner.sh
+++ IpScanner.sh
@@ -5,6 +5,6 @@ echo "You forgot ip address"
 echo "Syntax: ./IpScanner.sh 192.168.4"
 else
 
-for ip in `seq 1 254 `: do
+for ip in `seq 1 254`: do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
