ef89a6daa36ce53c5a33fa540bfefec6584f155a
mangalan <57430796+mangalan516@users.noreply.github.com>
2022-06-23 14:00:09 +0530

    Created IpScanner.sh
    The script file for scanning the ip addresses within the network.

---
+++ IpScanner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [$1" ==""]
+then
+echo "You forgot ip address"
+echo "Syntax: ./IpScanner.sh 192.168.4"
+else
+
+for ip in `seq 1 254 `: do
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+done
